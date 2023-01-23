f15966e55c52adabe4802e23354d59ffbf1e82a8
alextsyg <innempaya@gmail.com>
2023-01-23 10:31:28 +0300

    webpack config for project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './styles.css';
+
+const App = () => {
+    return (
+        <div className={styles.content}>
+            <div className={styles.label}>
+                Create React App Without CRA 123
+            </div>
+        </div>
+    )
+}
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const production = process.env.NODE_ENV === 'production';
+
+
+module.exports = {
+    entry: { myAppName: path.resolve(__dirname, "./src/index.js") },
+    output: {
+        path: path.resolve(__dirname, "./dist"),
+        filename: production ? '[name].[contenthash].js' : '[name].js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ["babel-loader"],
+            },
+            {
+                test: /\.css$/i,
+                exclude: /node_modules/,
+                use: [
+                    production ? MiniCssExtractPlugin.loader : 'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true,
+                            sourceMap: !production
+                        }
+                    },
+                ]
+            },
+        ],
+    },
+    resolve: {
+        extensions: ["*", ".js", ".jsx", ".css", ".scss"],
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: "Webpack & React",
+            template: "./public/index.html",
+        }),
+        new MiniCssExtractPlugin({
+            filename: production ? '[name].[contenthash].css' : '[name].css',
+        }),
+    ],
+    devServer: {
+        port: 3005,
+    },
+    mode: production ? 'production' : 'development'
+};
