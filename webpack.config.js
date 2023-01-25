01753143234b66dbc5f674432d5760e0f9ee9aad
alextsyg <innempaya@gmail.com>
2023-01-25 16:23:17 +0300

    edit/fix webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,56 +1,46 @@
-const webpack = require("webpack");
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-
-const production = process.env.NODE_ENV === 'production';
-
 
 module.exports = {
-    entry: { myAppName: path.resolve(__dirname, "./src/index.js") },
-    output: {
-        path: path.resolve(__dirname, "./dist"),
-        filename: production ? '[name].[contenthash].js' : '[name].js',
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "public", "index.html"),
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
     },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: ["babel-loader"],
+    port: 3005,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1,
+              modules: true,
             },
-            {
-                test: /\.css$/i,
-                exclude: /node_modules/,
-                use: [
-                    production ? MiniCssExtractPlugin.loader : 'style-loader',
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            modules: true,
-                            sourceMap: !production
-                        }
-                    },
-                ]
-            },
+          },
         ],
-    },
-    resolve: {
-        extensions: ["*", ".js", ".jsx", ".css", ".scss"],
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            title: "Webpack & React",
-            template: "./public/index.html",
-        }),
-        new MiniCssExtractPlugin({
-            filename: production ? '[name].[contenthash].css' : '[name].css',
-        }),
+      },
     ],
-    devServer: {
-        port: 3005,
-    },
-    mode: production ? 'production' : 'development'
+  },
+  resolve: {
+    extensions: ["*", ".js", ".jsx", ".css"],
+  },
 };
