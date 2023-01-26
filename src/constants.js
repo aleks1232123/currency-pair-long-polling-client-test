54717673dd8603098afe976c0d316a0aba297f4c
alextsyg <innempaya@gmail.com>
2023-01-26 16:03:36 +0300

    constant values using for project

---
+++ src/constants.js
@@ -0,0 +1,12 @@
+const SOURCES = ["first", "second", "third"]; //названия 3 источников данных
+const URL = "http://localhost:3000/api/v1"; //api url
+const ROW_NAMES = [
+  "RUB/CUPCAKE",
+  "USD/CUPCAKE",
+  "EUR/CUPCAKE",
+  "RUB/USD",
+  "RUB/EUR",
+  "EUR/USD",
+]; // названия строк в таблице
+
+export { SOURCES, URL, ROW_NAMES };
